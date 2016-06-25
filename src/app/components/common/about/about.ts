efaa41b435c0fb6b517759249b180def696bedb5
Kyle Clark <kyle@Kyles-MacBook-Pro.local>
2016-06-25 15:43:33 -0700

    Saving latest progress in design updates

---
+++ src/app/components/common/about/about.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  directives: [],
+  pipes: [],
+  styles: [require('./about.scss')],
+  template: require('./about.html')
+})
+
+export class About {}
--- src/app/components/routes/home/home.ts
+++ src/app/components/routes/home/home.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { Herospace } from 'components/common/herospace/herospace';
+import { About } from 'components/common/about/about';
 import { ProjectsList } from 'components/common/projects/list/projects-list';
 import PersonalDetails from 'services/data-store/personal-details';
 import Projects from 'services/data-store/projects';
@@ -9,6 +10,7 @@ import Musings from 'services/data-store/musings';
   selector: 'home',
   directives: [
     Herospace,
+    About,
     ProjectsList
   ],
   pipes: [],
